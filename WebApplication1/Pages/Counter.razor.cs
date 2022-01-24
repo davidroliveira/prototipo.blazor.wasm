f3cf98183e07ddd905bdc6077639d833bf9675c4
David Oliveira <david.theos@outlook.com.br>
2022-01-24 18:10:10 -0300

    Commit initial

---
+++ WebApplication1/Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace WebApplication1.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        [Parameter]
+        public int Incremento { get; set; } = 1;
+
+        private void IncrementCount()
+        {
+            currentCount += Incremento;
+        }
+    }
+}
